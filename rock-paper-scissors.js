fe4dd7533d4fcae27c747b003833a12463fc14be
Zach Coan <80611996+zcoan3@users.noreply.github.com>
2023-04-18 15:06:23 -0700

    Create game that plays one round of rock, paper, scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,3 +13,38 @@ function getComputerChoice() {
   // Return weapon
   return getWeapon;
 }
+
+
+
+// Compare the values
+// Return a winner based on rock > scissors, scissors > paper, paper > rock
+
+
+function playRoundOfGame(playerSelection, computerSelection) {
+  // Get a value for the users weapon based on input from the user
+  let playerWeapon = prompt("What will it be? Rock, Paper, or Scissors?").toUpperCase();
+  // Get a value for the computers weapon based on getComputerChoice
+  let computerWeapon = getComputerChoice().toUpperCase();
+  // Compare the values
+  let result;
+  if (playerWeapon === "ROCK" && computerWeapon === "ROCK") {
+    result = alert("It's a tie! We both played Rock.");
+  } else if (playerWeapon === "ROCK" && computerWeapon === "PAPER") {
+    result = alert("You lose! Paper beats Rock. Idoit.");
+  } else if (playerWeapon === "ROCK" && computerWeapon === "SCISSORS") {
+    result = alert("You won! Rock beats Scissors! I'm ruined.");
+  } else if (playerWeapon === "SCISSORS" && computerWeapon === "SCISSORS") {
+    result = alert("It's a tie! We both played Scissors.");
+  } else if (playerWeapon === "SCISSORS" && computerWeapon === "ROCK") {
+    result = alert("You lose! Rock beats Scissors. Idoit.");
+  } else if (playerWeapon === "SCISSORS" && computerWeapon === "PAPER") {
+    result = alert("You won! Scissors beats Paper! I'm ruined.");
+  } else if (playerWeapon === "PAPER" && computerWeapon === "PAPER") {
+    result = alert("It's a tie! We both played Paper.");
+  } else if (playerWeapon === "PAPER" && computerWeapon === "ROCK") {
+    result = alert("You won! Paper beats Rock! I'm ruined.");
+  } else if (playerWeapon === "PAPER" && computerWeapon === "SCISSORS") {
+    result = alert("You lose! Scissors beats Paper. Idoit.");
+  }
+  return result;
+}
